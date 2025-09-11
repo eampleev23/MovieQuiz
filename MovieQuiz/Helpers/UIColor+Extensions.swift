0a5aa3e687a6dcc8939559275e898350a4ce6489
ampleev.com <e@mpleev.com>
2025-09-11 08:01:37 +0300

    final

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+enum YPColors {
+    static var green: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+    static var red: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var black: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var background: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var gray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var white: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+}
