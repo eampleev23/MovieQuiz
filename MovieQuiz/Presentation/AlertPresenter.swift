2ed48a2112751809077557b156c0391e98079d18
ampleev.com <e@mpleev.com>
2025-09-22 06:36:06 +0300

    remove redundant comments

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -10,22 +10,12 @@ import UIKit
 
 final class AlertPresenter {
     
-    // Таким образом, название метода осталось как в MovieQuizViewController, но
-    // если раньше получали только QuizResultsViewModel, то сейчас получаем UIViewController и модель AlertModel.
-    
     func show(in vc: UIViewController, model:AlertModel) {
-        // по классике создаем алерт и берем заголовок и сообщение из модели
         let alert = UIAlertController(title: model.title, message: model.message, preferredStyle: .alert)
-        
-        // по классике создаем кнопку и программируем вызов замыкания в результате нажатия
         let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
             model.completion()
         }
-        
-        // связываем алерт с экшеном
         alert.addAction(action)
-        
-        // показываем алерт
         vc.present(alert, animated: true, completion: nil)
     }
 
